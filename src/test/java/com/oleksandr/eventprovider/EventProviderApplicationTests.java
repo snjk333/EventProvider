a13dbaef04d7675b881b199d008a65121f7c8db4
Kulbit Oleksandr <kulbitsanya0@gmail.com>
2025-09-15 16:42:10 +0200

    Start Changes

--- src/test/java/com/oleksandr/eventprovider/EventProviderApplicationTests.java
+++ src/test/java/com/oleksandr/eventprovider/EventProviderApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class EventProviderApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
